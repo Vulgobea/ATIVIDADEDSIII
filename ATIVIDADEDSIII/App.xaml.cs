e82ef23d25e9637d17194ee59289a5cae0e7341c
VulgoBea <fernandesbeatriz99@outlook.com>
2025-08-31 23:42:45 -0300

    efetuar atualização

--- ATIVIDADEDSIII/App.xaml.cs
+++ ATIVIDADEDSIII/App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace ATIVIDADEDSIII
+﻿using ATIVIDADEDSIII.Helpers;
+
+namespace ATIVIDADEDSIII
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
