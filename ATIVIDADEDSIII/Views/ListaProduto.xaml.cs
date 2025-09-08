293feb4ea59aedb76ebdf12c1b2abd063e702d1f
VulgoBea <fernandesbeatriz99@outlook.com>
2025-09-07 21:40:28 -0300

    Finalização do básico do aplicativo - remover e selecionar itens da lista

--- ATIVIDADEDSIII/Views/ListaProduto.xaml.cs
+++ ATIVIDADEDSIII/Views/ListaProduto.xaml.cs
@@ -14,11 +14,18 @@ public partial class ListaProduto : ContentPage
 		lst_produtos.ItemsSource = lista;
     }
 
-	protected async override void OnAppearing()
-	{
-		List<Produto> tmp = await App.Db.GetAll();
+    protected async override void OnAppearing()
+    {
+        try
+        {
+            List<Produto> tmp = await App.Db.GetAll();
 
-		tmp.ForEach( i => lista.Add(i));
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
@@ -35,13 +42,21 @@ public partial class ListaProduto : ContentPage
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
+        try
+        {
+            string q = e.NewTextValue;
 
-		lista.Clear();
+            lista.Clear();
 
-        List<Produto> tmp = await App.Db.Search(q);
+            List<Produto> tmp = await App.Db.Search(q);
 
-        tmp.ForEach(i => lista.Add(i));
+            tmp.ForEach(i => lista.Add(i));
+        }
+
+        catch (Exception ex)
+        {
+           await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
@@ -59,18 +74,37 @@ public partial class ListaProduto : ContentPage
         {
             MenuItem selecionado = sender as MenuItem;
 
-            Produto produto = selecionado.BindingContext as Produto;
-            bool confirm = await DisplayAlert("Deseja Excluir?", $"Remover {produto.Descricao}?", "Sim", "Não");
+            Produto p = selecionado.BindingContext as Produto;
+            bool confirm = await DisplayAlert
+                ("Tem certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
 
             if (confirm)
             {
-                await App.Db.Delete(produto.Id);
-                lista.Remove(produto);
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
             }
         }
         catch (Exception ex)
         {
             await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto;
+
+            Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p,
+            });
+        }
+
+         catch (Exception ex)
+		{
+            DisplayAlert("Ops", ex.Message, "OK");
         }
     }
 }
